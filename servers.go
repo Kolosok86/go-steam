92ba304950b33d29312d571a613c74af4ab0aa13
vladislav <kolosok186@gmail.com>
2025-12-25 17:34:21 +0100

    chore: update servers

--- servers.go
+++ servers.go
@@ -9,32 +9,30 @@ import (
 
 // CMServers contains a list of worldwide servers
 //
-// curl -s "https://api.steampowered.com/ISteamDirectory/GetCMListForConnect/v1/?key=$STEAM_API_KEY&format=json&celllid=1&cmtype=netfilter" | jq -r '.response.serverlist[] | .endpoint' | awk 'BEGIN {print "var CMServers = []string{"} {print "  \""$1"\","} END {print "}"}'
+// curl -s "https://api.steampowered.com/ISteamDirectory/GetCMListForConnect/v1?format=json&celllid=0&cmtype=netfilter" | jq -r '.response.serverlist[] | .endpoint' | awk 'BEGIN {print "var CMServers = []string{"} {print "  \""$1"\","} END {print "}"}'
 var CMServers = []string{
-	"155.133.248.38:27017",
-	"155.133.248.39:27017",
-	"155.133.226.76:27017",
 	"162.254.197.40:27017",
-	"162.254.197.54:27017",
+	"146.66.155.54:27017",
+	"146.66.155.38:27017",
 	"162.254.197.38:27017",
-	"155.133.226.74:27017",
-	"185.25.182.20:27017",
-	"185.25.182.52:27017",
-	"155.133.226.78:27017",
 	"162.254.197.39:27017",
+	"162.254.197.54:27017",
+	"155.133.226.76:27017",
+	"155.133.226.78:27017",
+	"155.133.226.74:27017",
 	"155.133.226.75:27017",
-	"155.133.252.54:27017",
 	"155.133.252.40:27017",
-	"162.254.198.104:27017",
-	"155.133.252.39:27017",
 	"162.254.198.46:27017",
 	"162.254.198.44:27017",
-	"146.66.155.54:27017",
-	"162.254.196.68:27017",
-	"162.254.196.84:27017",
-	"146.66.155.38:27017",
-	"162.254.196.83:27017",
-	"162.254.196.67:27017",
+	"155.133.252.54:27017",
+	"155.133.252.39:27017",
+	"162.254.198.104:27017",
+	"155.133.248.38:27017",
+	"155.133.248.39:27017",
+	"185.25.182.52:27017",
+	"185.25.182.20:27017",
+	"155.133.230.50:27017",
+	"155.133.230.34:27017",
 }
 
 // GetRandomCM returns a random server from a built-in IP list.
